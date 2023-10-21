60d54cb80a2343ffc8021e0178a7fbb04cc69ece
Kenta Kase <kesin1202000@gmail.com>
2023-10-21 13:34:30 +0000

    deno fmt

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { debug, getInput, info } from "npm:@actions/core@1.10.1";
 import { writeSummary } from "./github.ts";
 
 const main = async () => {
-  info("Start main process")
+  info("Start main process");
   const message = getInput("message", { required: true });
 
-  debug(message)
-  await writeSummary("main", message)
+  debug(message);
+  await writeSummary("main", message);
 
-  info("Complete main process")
-}
-main()
+  info("Complete main process");
+};
+main();
--- src/post.ts
+++ src/post.ts
@@ -2,11 +2,11 @@ import { debug, info } from "npm:@actions/core@1.10.1";
 import { writeSummary } from "./github.ts";
 
 const main = async () => {
-  info("Start post process")
-  debug("Debug message")
+  info("Start post process");
+  debug("Debug message");
 
-  await writeSummary("post", "This is a post process summary")
+  await writeSummary("post", "This is a post process summary");
 
-  info("Complete post process")
-}
-main()
+  info("Complete post process");
+};
+main();
