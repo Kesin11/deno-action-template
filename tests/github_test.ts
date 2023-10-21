90c57c73b186f735e353d12d5a767aa8df087bbd
Kenta Kase <kesin1202000@gmail.com>
2023-10-21 13:43:12 +0000

    Bugfix test script write to real GITHUB_STEP_SUMMARY

--- tests/github_test.ts
+++ tests/github_test.ts
@@ -1,13 +1,10 @@
 import { assertEquals } from "https://deno.land/std@0.204.0/assert/mod.ts";
 import { writeSummary } from "../src/github.ts";
 
-if (Deno.env.get("GITHUB_STEP_SUMMARY") === undefined) {
-  const tempFilePath = "/tmp/summary.md";
-  Deno.env.set("GITHUB_STEP_SUMMARY", tempFilePath);
-
-  await Deno.open(tempFilePath, { create: true, write: true });
-  await Deno.truncate(tempFilePath);
-}
+const tempFilePath = "/tmp/summary.md";
+Deno.env.set("GITHUB_STEP_SUMMARY", tempFilePath);
+await Deno.open(tempFilePath, { create: true, write: true });
+await Deno.truncate(tempFilePath);
 
 Deno.test("github", async (t) => {
   await t.step("write GITHUB_SUMMARY", async () => {
