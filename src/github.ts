b98fab20fff245d6788ad086cf97d580231d622c
Kenta Kase <kesin1202000@gmail.com>
2023-11-27 14:53:50 +0000

    debug: raise error code

--- src/github.ts
+++ src/github.ts
@@ -2,5 +2,6 @@ import { summary } from "npm:@actions/core@1.10.1";
 
 export async function writeSummary(header: string, message: string) {
   const md = `## ${header}\n\n${message}`;
+  throw new Error("Raise error");
   await summary.addRaw(md).write();
 }
